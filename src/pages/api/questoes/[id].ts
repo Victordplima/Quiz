f250ec7fb3461260ef3a08aae1661dd2d7d7ac4b
Victor <victorlima0302@gmail.com>
2025-01-05 17:45:51 -0300

    feat: adiciona a logica de puxar a questao pelo id

--- src/pages/api/questoes/[id].ts
+++ src/pages/api/questoes/[id].ts
@@ -2,5 +2,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 import questoes from "../bancoDeQuestoes";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json(questoes[0].converterParaObjeto());
+    if (!req.query.id) {
+        return res.status(400).json({ error: "ID não fornecido" });
+    }
+
+    const idSelecionado = +req.query.id;
+
+    const unicaQuestaoOuNada = questoes.filter(
+        (questao) => questao.id === idSelecionado
+    );
+
+    if (unicaQuestaoOuNada.length === 1) {
+        const questaoSelecionada = unicaQuestaoOuNada[0];
+        res.status(200).json(questaoSelecionada.converterParaObjeto());
+    } else {
+        res.status(204).end();
+    }
 }
