d07d55bfc156bcfd17f14ad9bc5f213712077cca
Victor <victorlima0302@gmail.com>
2025-01-11 20:17:02 -0300

    feat: adiciona a tela Resultado com seus componentes de estatistica

---
+++ components/Estatistica.tsx
@@ -0,0 +1,27 @@
+import styles from "../src/styles/Estatistica.module.css";
+
+interface EstatisticaProps {
+    valor: any;
+    texto: string;
+    corFundo?: string;
+    corFonte?: string;
+}
+
+export default function Estatistica(props: EstatisticaProps) {
+    return (
+        <div className={styles.estatistica}>
+            <div
+                className={styles.valor}
+                style={{
+                    backgroundColor: props.corFundo ?? "#6848fe",
+                    color: props.corFonte ?? "#37e9bb",
+                }}
+            >
+                {props.valor}
+            </div>
+            <div className={styles.texto}>
+                {props.texto}
+            </div>
+        </div>
+    );
+}
--- src/pages/resultado.tsx
+++ src/pages/resultado.tsx
@@ -1,7 +1,24 @@
-export default function resultado (){
-    return(
-        <div>
-            <h1>Resultado</h1>
+import Botao from "../../components/Botao";
+import Estatistica from "../../components/Estatistica";
+import styles from "../styles/Resultado.module.css";
+import { useRouter } from "next/router";
+
+export default function resultado() {
+    const router = useRouter();
+
+    const total = router.query.total ? +router.query.total : 0;
+    const certas = router.query.certas ? +router.query.certas : 0;
+    const percentual = Math.round((certas / total) * 100);
+
+    return (
+        <div className={styles.resultado}>
+            <h1>Resultados Finais</h1>
+            <div style={{ display: "flex" }}>
+                <Estatistica texto="Perguntas" valor={total} />
+                <Estatistica texto="Certas" valor={certas} />
+                <Estatistica texto="Percentual" valor={`${percentual}%`} />
+            </div>
+            <Botao href="/" texto="Tentar novamente"/>
         </div>
-    )
+    );
 }
