aa66c5462dc42a999b6b3618b0e8910cc26de2a5
Victor <victorlima0302@gmail.com>
2025-01-11 20:30:34 -0300

    feat: mudando o nome da aba pra quiz a adicionando fotos do projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Quiz</title>
+        <meta name="description" content="Um quiz divertido para testar seus conhecimentos!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
